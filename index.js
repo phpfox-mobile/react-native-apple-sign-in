4b408c299d26b8a0c3014eae493804a381286355
Lê Tuấn Tài <tuantai0625@gmail.com>
2019-12-02 16:13:28 +0700

    Fix import issue

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
-import { NativeModules ,requireNativeComponent ,Platform } from 'react-native';
+import React from 'react'
+import { NativeModules, requireNativeComponent, Platform } from 'react-native'
 
-const { AppleSignIn } = NativeModules;
+const { AppleSignIn } = NativeModules
 
-export const RNSignInWithAppleButton = requireNativeComponent('RNCSignInWithAppleButton');
+export const RNSignInWithAppleButton = requireNativeComponent('RNCSignInWithAppleButton')
 
 export const SignInWithAppleButton = (buttonStyle, callBack) => {
-  if(Platform.OS === 'ios'){
-    return <RNSignInWithAppleButton style={buttonStyle} onPress={async () => {
-    
-        await RNCAppleAuthentication.requestAsync({
-          scopes: [RNCAppleAuthentication.Scope.FULL_NAME, RNCAppleAuthentication.Scope.EMAIL],
-        }).then((response) => {
-          callBack(response) //Display response
-          }, (error) => {
-            callBack(error) //Display error
-           
-        });
-
-  }} />
-  }else{
-  return null
-
+  if (Platform.OS === 'ios') {
+    return <RNSignInWithAppleButton style={ buttonStyle } onPress={ async () => {
+      await AppleSignIn.requestAsync({
+        scopes: [AppleSignIn.Scope.FULL_NAME, AppleSignIn.Scope.EMAIL],
+      }).then((response) => {
+        callBack(response) //Display response
+      }, (error) => {
+        callBack(error) //Display error
+      })
+    } }/>
+  } else {
+    return null
   }
-   
 }
 
-
-export default AppleSignIn;
+export default AppleSignIn
