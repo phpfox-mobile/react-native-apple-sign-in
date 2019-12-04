95a7f180f69397e3ec5708cafd3d5cc635b1e194
Lê Tuấn Tài <tuantai0625@gmail.com>
2019-12-04 17:27:31 +0700

    Fix index.js

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 import React from 'react'
-import { NativeModules, requireNativeComponent, Platform } from 'react-native'
+import { NativeModules, requireNativeComponent, Platform , Platform } from 'react-native'
 
 const { AppleSignIn } = NativeModules
 
 export const RNSignInWithAppleButton = requireNativeComponent('RNCSignInWithAppleButton')
 
+const majorVersionIOS = parseInt(Platform.Version, 10);
+
 export const SignInWithAppleButton = (buttonStyle, callBack) => {
-  if (Platform.OS === 'ios') {
+  if (Platform.OS === 'ios' && majorVersionIOS >= 13) {
     return <RNSignInWithAppleButton style={ buttonStyle } onPress={ async () => {
       await AppleSignIn.requestAsync({
-        scopes: [AppleSignIn.Scope.FULL_NAME, AppleSignIn.Scope.EMAIL],
+        requestedScopes: [AppleSignIn.Scope.FULL_NAME, AppleSignIn.Scope.EMAIL],
       }).then((response) => {
         callBack(response) //Display response
       }, (error) => {
